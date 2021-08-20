509c6c9421d852413c85d584a708cffab14a80df
Özgür Yalçın <ozguryalcin@outlook.com>
2021-08-20 16:14:18 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+
+	paycell "github.com/ozgur-soft/paycell/src"
+)
+
 func main() {
+	api := &paycell.API{"TEST"} // "PROD","TEST"
+	request := new(paycell.Request)
+	request.PaymentMethods.MSisdn = "905325323232"              // Müşteri telefon numarası
+	request.PaymentMethods.Header.ClientIPAddress = "127.0.0.1" // Müşteri ip adresi
+	response := api.GetPaymentMethods(request)
+	pretty, _ := json.MarshalIndent(response.PaymentMethods, " ", "\t")
+	fmt.Println(string(pretty))
 }
